eabc226a007dad58812e6ef4cde986073702f70a
Luka Mugosa <luka.mugosa@amplitudo.me>
2020-10-18 16:36:30 +0200

    adds speciality for vet

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
